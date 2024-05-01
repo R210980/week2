aff3882c0058687d2d503bb5b2c57b61f946f4ca
R210980 <159228280+R210980@users.noreply.github.com>
2024-05-01 15:17:13 +0530

    Add files via upload

---
+++ addsine.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,10,0.01)
+x2 = np.arange(0,20,0.02)
+
+sin1 = np.sin(x)
+sin2 = np.sin(x2)
+
+x2 /= 2
+
+sin3 = sin1+sin2
+
+plt.plot(x,sin3)
+plt.show()
---
+++ subsine.m
@@ -0,0 +1,11 @@
+x = 0:0.01:10;
+x2 = 0:0.02:20;
+
+sin1 = sin(x);
+sin2 = sin(x2);
+
+x2 /= 2;
+
+sin3 = sin1 - sin2;
+
+plot(x, sin3);
